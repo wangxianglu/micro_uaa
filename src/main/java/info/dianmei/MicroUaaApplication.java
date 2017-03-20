4a0b95aaed49737547ed2adb8c53ce9870f3fb24
wangxianglu <lu_action@qq.com>
2017-03-20 21:45:01 +0800

    first commit

---
+++ src/main/java/info/dianmei/MicroUaaApplication.java
@@ -0,0 +1,12 @@
+package info.dianmei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroUaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroUaaApplication.class, args);
+	}
+}
---
+++ src/test/java/info/dianmei/MicroUaaApplicationTests.java
@@ -0,0 +1,16 @@
+package info.dianmei;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroUaaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
