17cad5f88b851441ff95721612fefae092bd2bb0
wangxianglu <lu_action@qq.com>
2017-03-21 09:52:11 +0800

    add security configuration

--- src/main/java/info/dianmei/MicroUaaApplication.java
+++ src/main/java/info/dianmei/MicroUaaApplication.java
@@ -2,11 +2,32 @@ package info.dianmei;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.Ordered;
+import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class MicroUaaApplication {
+public class MicroUaaApplication extends WebMvcConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MicroUaaApplication.class, args);
+	}
+
+
+	@Bean
+	FilterRegistrationBean forwardedHeaderFilter() {
+		FilterRegistrationBean filterRegBean = new FilterRegistrationBean();
+		filterRegBean.setFilter(new ForwardedHeaderFilter());
+		filterRegBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+		return filterRegBean;
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/login").setViewName("login");
+		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
 	}
 }
